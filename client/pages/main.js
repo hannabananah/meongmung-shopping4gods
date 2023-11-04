37f3e2e81d46381782fa7f1a4af36928b96401e9
Dev-koo <devgiyun@gmail.com>
2023-11-05 02:19:41 +0900

    🔨 Refactor 메인페이지 리펙토링

--- client/pages/main.js
+++ client/pages/main.js
@@ -1,5 +1,58 @@
 import '../style.css';
 import '../index.css';
+import 'flowbite';
+const data = {
+  goods: [
+    {
+      id: 1,
+      productName: '장난감 공',
+      price: 8000,
+      image: '../images/Dog_ball.jpg',
+      cart: true,
+      order: 0,
+    },
+    {
+      id: 2,
+      productName: '딩동펫',
+      price: 10000,
+      image: '../images/02.png',
+      cart: false,
+      order: 0,
+    },
+    {
+      id: 3,
+      productName: '도기맨',
+      price: 12000,
+      image: '../images/03.png',
+      cart: false,
+      order: 0,
+    },
+    {
+      id: 4,
+      productName: '도기맨',
+      price: 12000,
+      image: '../images/03.png',
+      cart: false,
+      order: 0,
+    },
+    {
+      id: 5,
+      productName: '장난감 공',
+      price: 8000,
+      image: '../images/Dog_ball.jpg',
+      cart: true,
+      order: 0,
+    },
+    {
+      id: 6,
+      productName: '장난감 공',
+      price: 8000,
+      image: '../images/Dog_ball.jpg',
+      cart: true,
+      order: 0,
+    },
+  ],
+};
 
 export function init() {
   document.querySelector('#header-wrapper').innerHTML = `<header
@@ -35,3 +88,35 @@ export function init() {
 window.addEventListener('DOMContentLoaded', () => {
   init();
 });
+
+export function renderProducts() {
+  const productList = document.getElementById('product-list');
+
+  const products = data.goods; // JSON 데이터에서 제품 목록을 가져옴
+
+  // 가져온 데이터를 사용하여 동적으로 제품 목록 생성
+  products.forEach((product) => {
+    // 제품 항목을 생성하고 추가하는 코드
+    const productCard = document.createElement('div');
+    productCard.classList.add('product-card');
+    productCard.innerHTML = `
+    <div class="bg-white rounded-lg border border-gray-400 p-8">
+    <div class="relative overflow-hidden">
+            <a href='../detail/'>
+              <img class="object-cover z-2 w-full" src="${product.image}" alt="${product.productName}" />
+            </a>
+          <h2 class="text-xl z-2 font-bold text-gray-900 mt-4">${product.productName}</h2>
+          <div class="flex z-2 items-center justify-between mt-4">
+          <span class="text-gray-900 z-2 font-bold text-lg">${product.price}원</span>
+          <button data-action='add-to-cart' class="bg-gray-900 text-white z-2 py-2 px-4 rounded-full font-bold hover:bg-gray-800" data-product-id="${product.id}">장바구니</button>
+          </div>
+          </div>
+          <script src="data.json" type="home/javascript"></script>
+        `;
+
+    if (productList) {
+      productList.appendChild(productCard);
+    }
+  });
+}
+renderProducts();
