3fc87a3aa42ace21c3e40c9055687dc218cb362b
Dev-koo <devgiyun@gmail.com>
2023-11-05 02:12:11 +0900

    ⚙️ Setting 이미지 추가 및 config 설정

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,13 @@
+import flowbitePlugin from 'flowbite/plugin';
 /** @type {import('tailwindcss').Config} */
 export default {
-
-  content: ['./pages/**/*.html','./pages/**/*.js'],
+  content: [
+    './pages/**/*.html',
+    './pages/**/*.js',
+    './node_modules/flowbite/**/*.js',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [flowbitePlugin],
 };
