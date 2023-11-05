a94c750567564ccbde900ce0033326a3c74a5b76
[SW7] 홍소현 <hshyeon86@gmail.com>
2023-11-05 17:13:08 +0900

    ✨ Feat product 관련 api 추가

--- client/pages/detail/detail.js
+++ client/pages/detail/detail.js
@@ -1,11 +1,37 @@
 import '../../index.css';
 import { init } from '../main.js';
-import Swal from 'sweetalert2/dist/sweetalert2.js'
-
-import 'sweetalert2/src/sweetalert2.scss'
+import Swal from 'sweetalert2';
 
 init();
 
+const API_BASE_URL = import.meta.env.VITE_BASE_URL;
+const img = document.getElementById('img');
+const txtname = document.getElementById('productName');
+const txtcost = document.getElementById('cost');
+
+
+const id_query = new URLSearchParams(location.search).get("id"); //querystring으로 상품아이디 받아옴
+console.log(id_query);
+
+const getProduct= ()=> {
+    fetch(`${API_BASE_URL}/products/${id_query}` , {
+      method: 'GET',
+    })
+      .then((response) => 
+        response.json())
+      .then((data) => {
+        console.log(data);
+        loadItem(data);
+      })
+      .catch(error => console.log(error));
+  }
+  function loadItem(data){
+    img.src = data.img_url;
+    txtname.innerHTML = data.name;
+    txtcost.innerHTML = data.price;
+  }
+  
+getProduct();
 //수량 counter
 const target = document.getElementById('input');
 let value = Number(target.value);
@@ -29,10 +55,18 @@ const cartbtn = document.getElementById('cartbtn');
 const buybtn = document.getElementById('buybtn');
 
 cartbtn.addEventListener("click", function(){
+    localStorage.getItem('cart');
     //이미 장바구니에 있을 경우 예외처리
     // swal('잠시만요', '이미 장바구니에 추가되어 있습니다', 'warning').then(() => {
     //     // location.href = '../cart/';
     //    });
+    let product ={
+        id : product.id,
+      //  name: ,
+        quantity: target.value,
+    //    imgUrl:
+    }
+    localStorage.setItem('cart', product);
     new Swal('장바구니에 담겼습니다', '', 'success').then(() => {
         location.href = '/cart/';
       });
--- client/pages/main.js
+++ client/pages/main.js
@@ -1,58 +1,7 @@
 import '../style.css';
 import '../index.css';
 import 'flowbite';
-const data = {
-  goods: [
-    {
-      id: 1,
-      productName: '장난감 공',
-      price: 8000,
-      image: '../images/Dog_ball.jpg',
-      cart: true,
-      order: 0,
-    },
-    {
-      id: 2,
-      productName: '딩동펫',
-      price: 10000,
-      image: '../images/02.png',
-      cart: false,
-      order: 0,
-    },
-    {
-      id: 3,
-      productName: '도기맨',
-      price: 12000,
-      image: '../images/03.png',
-      cart: false,
-      order: 0,
-    },
-    {
-      id: 4,
-      productName: '도기맨',
-      price: 12000,
-      image: '../images/03.png',
-      cart: false,
-      order: 0,
-    },
-    {
-      id: 5,
-      productName: '장난감 공',
-      price: 8000,
-      image: '../images/Dog_ball.jpg',
-      cart: true,
-      order: 0,
-    },
-    {
-      id: 6,
-      productName: '장난감 공',
-      price: 8000,
-      image: '../images/Dog_ball.jpg',
-      cart: true,
-      order: 0,
-    },
-  ],
-};
+
 
 export function init() {
   document.querySelector('#header-wrapper').innerHTML = `<header
@@ -89,10 +38,10 @@ window.addEventListener('DOMContentLoaded', () => {
   init();
 });
 
-export function renderProducts() {
+export function renderProducts(data) {
   const productList = document.getElementById('product-list');
 
-  const products = data.goods; // JSON 데이터에서 제품 목록을 가져옴
+  const products = data.productList; // JSON 데이터에서 제품 목록을 가져옴
 
   // 가져온 데이터를 사용하여 동적으로 제품 목록 생성
   products.forEach((product) => {
@@ -100,23 +49,37 @@ export function renderProducts() {
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
     productCard.innerHTML = `
-    <div class="bg-white rounded-lg border border-gray-400 p-8">
+    <div class="bg-white rounded-lg shadow-lg  p-8" id="product-${product._id}">
     <div class="relative overflow-hidden">
-            <a href='../detail/'>
-              <img class="object-cover z-2 w-full" src="${product.image}" alt="${product.productName}" />
-            </a>
-          <h2 class="text-xl z-2 font-bold text-gray-900 mt-4">${product.productName}</h2>
+          <img class="object-cover z-2 w-full" src="${product.img_url}" alt="${product.name}" />
+          <h2 class="text-xl z-2 font-bold text-gray-900 mt-4">${product.name}</h2>
           <div class="flex z-2 items-center justify-between mt-4">
           <span class="text-gray-900 z-2 font-bold text-lg">${product.price}원</span>
-          <button data-action='add-to-cart' class="bg-gray-900 text-white z-2 py-2 px-4 rounded-full font-bold hover:bg-gray-800" data-product-id="${product.id}">장바구니</button>
+          <button data-action='add-to-cart' class="bg-blue-900 text-white z-2 py-2 px-4 rounded-full font-bold hover:bg-gray-800" data-product-id="${product.id}">장바구니</button>
           </div>
           </div>
-          <script src="data.json" type="home/javascript"></script>
         `;
-
     if (productList) {
       productList.appendChild(productCard);
     }
+    const card = document.querySelector(`#product-${product._id}`)
+    card.addEventListener('click',function(){
+      location.href = `/detail/?id=${product._id}`;
+    })
   });
 }
-renderProducts();
+const API_BASE_URL = import.meta.env.VITE_BASE_URL;
+
+const getProducts= ()=> {
+  fetch(`${API_BASE_URL}/products/` , {
+    method: 'GET',
+  })
+    .then((response) => 
+      response.json())
+    .then((data) => {
+      console.log(data);
+      renderProducts(data);
+    })
+    .catch(error => console.log(error));
+}
+getProducts();
