3b8697d4ec3a6d88ee23dcbd06427fc02cadbccc
[SW7] 홍소현 <hshyeon86@gmail.com>
2023-11-05 14:22:11 +0900

    ✨ Feat 도로명주소api

--- client/pages/order/order.js
+++ client/pages/order/order.js
@@ -1,4 +1,23 @@
 import '../../index.css';
 import { init } from '../main.js';
 
+
 init();
+
+const addressBtn = document.getElementById('getAddress');
+const addressNum = document.getElementById('addressNum');
+const address = document.getElementById('address');
+
+//daum 주소 입력받기
+addressBtn.addEventListener('click', function() {
+    new daum.Postcode({
+        oncomplete: function(data) {
+
+            
+            addressNum.value = data.zonecode;    
+            address.value = data.address;
+
+        }
+    }).open();
+})
+
