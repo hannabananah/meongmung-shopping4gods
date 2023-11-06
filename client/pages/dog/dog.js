8ce6e595419aa85a996d7a1918afa6800cb9943b
[SW7] 홍소현 <hshyeon86@gmail.com>
2023-11-07 01:21:10 +0900

    ✨ Feat 강아지 정보 페이지 추가

---
+++ client/pages/dog/dog.js
@@ -0,0 +1,70 @@
+import '../../index.css';
+import { init } from '../main.js';
+import Swal from 'sweetalert2';
+
+const API_BASE_URL = import.meta.env.VITE_BASE_URL;
+const btn = document.querySelector('form');
+
+const token = localStorage.getItem('token');
+let userId = localStorage.getItem('userId');
+
+
+const dogId = document.getElementById('dogid');
+const name = document.getElementById('name');
+const size = document.getElementById('size');
+const age = document.getElementById('age');
+
+
+btn.addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    postDog();
+  
+  });
+  
+  const postDog = () => {
+    fetch(`${API_BASE_URL}/dogs` , {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({
+         dogId: dogId.value,
+         userId: userId,
+         name: name.value,
+         size: size.value,
+         age: age.value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        
+      })
+      .catch(error => console.log(error));;
+  };
+
+
+  const getDog = () => {
+    fetch(`${API_BASE_URL}dogs` , {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({
+         dogId: dogId.value,
+         userId: userId,
+         name: name.value,
+         size: size.value,
+         age: age.value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        
+      })
+      .catch(error => console.log(error));;
+  };
