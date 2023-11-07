b3408f5d9d06e179cdc38451a9d6a7a115ba9002
[SW7] 강한나 <hanna0175@gmail.com>
2023-11-08 01:13:06 +0900

    ✨ Feat adminAddProduct style 입히기

--- client/pages/adminAddProduct/adminAddProduct.js
+++ client/pages/adminAddProduct/adminAddProduct.js
@@ -2,3 +2,19 @@ import '../../index.css';
 import { init } from '../main.js';
 
 init();
+
+function numberWithCommas(x) {
+  return x.replace(/\D/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+window.onload = function () {
+  const priceInput = document.getElementById('price');
+
+  priceInput.addEventListener('input', function (e) {
+    const originalValue = e.target.value;
+    const cleanedValue = originalValue.replace(/[^0-9,]/g, '');
+    const formattedValue = numberWithCommas(cleanedValue);
+
+    e.target.value = formattedValue;
+  });
+};
