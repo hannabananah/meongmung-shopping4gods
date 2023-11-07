5d70ef76e3a544077c6733ab98b2ba39616b0308
[SW7] 채유림 <yulim1510@gmail.com>
2023-11-07 05:40:27 +0000

    Feat fe/order

--- client/pages/cart/cart.js
+++ client/pages/cart/cart.js
@@ -2,3 +2,239 @@ import '../../index.css';
 import { init } from '../main.js';
 
 init();
+
+const cartContainer = document.querySelector('.cart-container');
+const cartTotalPrice = document.querySelector('.total-price');
+const cartBox = document.querySelector('.cart-container-box');
+const cartEmpty = document.querySelector('.empty');
+
+let saveCartGoods = localStorage.getItem('cartList')
+  ? JSON.parse(localStorage.getItem('cartList'))
+  : [];
+
+function cartCreateHTML(product) {
+  return `
+    <div class="flex border-t border-gray-300 items-center  ">
+    <div class=" lg:w-6/12 py-2 ">
+      <div class="flex items-center">
+        <div class="md:w-1/3">
+            <a href="#">
+              <img
+                src="${product.image}"
+                alt="${product.productName}"
+                class="md:h-24 md:w-24 "
+              />
+            </a>
+        </div>
+        <div class="w-2/3 px-4">
+          <h2 class="mb-2 text-xl font-bold">
+            <a
+              href="#"
+              class="text-lg font-bold hover:text-gray-200"
+              >${product.productName}</a
+            >
+          </h2>
+         <button
+         class="item-remove text-gray-500 hover:text-gray-200"
+          data-id="${product.id}"
+        >
+          삭제
+        </button>
+        
+        </div>
+      </div>
+    </div>
+    <div class=" px-4  lg:w-2/12">
+      <p class="text-lg font-bold text-blue-500">${product.price}</p>
+    </div>
+    <div class="custom-number-input">
+          <button
+            class="count-minus py-2 hover:text-gray-700"
+            data-value="minus"
+            data-id=${product.id}
+          >
+          <span class="m-auto text-2xl font">−</span>
+          </button>
+          <input
+            type="number"
+            class="count w-10 px-1 py-1 items-center text-center border-1 rounded-md bg-gray-50"
+            value=${product.order}
+          />
+          <button
+            class="count-plus py-2 hover:text-gray-700"
+            data-value="plus"
+            data-id=${product.id}
+          >
+          <span class="m-auto text-2xl font">+</span>
+          </button>
+          </div>
+    <div class=" px-4 ml-auto lg:w-2/12">
+      <p class="single-total-price text-lg font-bold text-center text-blue-500">
+        ${(product.price * product.order).toLocaleString()}
+      </p>
+    </div>
+  </div>
+ `;
+}
+
+//total price
+function totalPrice() {
+  const priceBox = saveCartGoods.reduce((prev, curr) => {
+    return prev + curr.price * curr.order;
+  }, 0);
+  if (cartTotalPrice) {
+    cartTotalPrice.innerHTML = priceBox.toLocaleString();
+  }
+  const totalcosts = document.querySelectorAll('.total-price');
+  totalcosts.forEach((totalCost) => {
+    totalCost.innerHTML = priceBox.toLocaleString();
+    if (saveCartGoods.length === 0) {
+      totalCost.innerHTML = 0;
+    }
+  });
+}
+window.addEventListener('load', totalPrice);
+
+//cart total number of goods
+export function totalCartCount() {
+  const countBox = saveCartGoods.reduce((prev, curr) => {
+    return prev + curr.order;
+  }, 0);
+  const totalCounts = document.querySelectorAll('.top-cart-count');
+  totalCounts.forEach((totalCount) => {
+    totalCount.innerHTML = countBox;
+    if (saveCartGoods.length === 0) {
+      totalCount.innerHTML = 0;
+    }
+  });
+}
+
+window.addEventListener('load', totalCartCount);
+
+// cart-page paint
+export function paintCartPage() {
+  const loadCartGoods = localStorage.getItem('cartList');
+  if (cartContainer !== null) {
+    cartContainer.innerHTML = JSON.parse(loadCartGoods)
+      .map((product) => cartCreateHTML(product))
+      .join('');
+    if (cartContainer.children.length !== 0) {
+      cartEmpty.classList.add('hidden');
+      cartBox.classList.remove('hidden');
+    }
+  }
+  totalPrice();
+}
+
+window.addEventListener('load', paintCartPage);
+
+//save cart
+export function saveCart(saveCartGoods) {
+  localStorage.setItem('cartList', JSON.stringify(saveCartGoods));
+}
+
+// 모든 장바구니 버튼에 대한 클릭 핸들러
+export const buttonClickHandler = function () {
+  // 클릭한 버튼의 데이터(product-id)를 가져옴
+  const productId = event.target.dataset.productId;
+
+  // 해당 제품을 찾아냄
+  const selectedProduct = data.goods.find(
+    (product) => product.id === parseInt(productId),
+  );
+
+  // 이미 장바구니에 있는 상품인지 확인
+  if (saveCartGoods.some((product) => product.id === selectedProduct.id)) {
+    console.log('이미 장바구니에 있는 상품입니다.');
+    alert('장바구니에 있는 상품입니다.');
+  } else {
+    alert('장바구니에 담았습니다.');
+    selectedProduct.cart = true;
+    selectedProduct.order = 1;
+    saveCartGoods.push(selectedProduct);
+    console.log('장바구니에 추가');
+  }
+  // 장바구니 정보를 localStorage에 업데이트
+  localStorage.setItem('cartList', JSON.stringify(saveCartGoods));
+  saveCart(saveCartGoods);
+  totalCartCount();
+  totalPrice();
+  console.log('저장완료');
+};
+
+// 모든 장바구니 버튼에 클릭 핸들러를 추가
+export const cartbtns = document.querySelectorAll('cart-add');
+cartbtns.forEach((cartbtn) => {
+  cartbtn.addEventListener('click', buttonClickHandler);
+});
+
+//delete cart
+function deleteCart(e) {
+  const cartRemoveBtns = document.querySelectorAll('.item-remove');
+  cartRemoveBtns.forEach((cartRemoveBtn) => {
+    if (e.target === cartRemoveBtn) {
+      const cleanCart = saveCartGoods.findIndex((item) => {
+        return item.id === parseInt(cartRemoveBtn.dataset.id);
+      });
+      //cart-storage에서 삭제
+      saveCartGoods.splice(cleanCart, 1);
+      //cart-page에서 삭제
+      cartContainer.removeChild(cartContainer.children[cleanCart]);
+      //변경사항 저장
+      saveCart(saveCartGoods);
+      totalPrice();
+      totalCartCount();
+    }
+  });
+  if (cartContainer.children.length === 0) {
+    cartEmpty.classList.remove('hidden');
+    cartBox.classList.add('hidden');
+  }
+}
+
+//single goods price and count
+function singleGoodsControl(e, plusMinusBtns) {
+  const goodsCount = document.querySelectorAll('.count');
+  const singleGoodsPrice = document.querySelectorAll('.single-total-price');
+
+  plusMinusBtns.forEach((plusMinusBtn) => {
+    if (e.target.parentNode === plusMinusBtn) {
+      const cartdataId = saveCartGoods.findIndex((item) => {
+        return item.id === parseInt(plusMinusBtn.dataset.id);
+      });
+      const pickGoods = saveCartGoods[cartdataId];
+      //cart-storage에서 수량 증감
+      if (plusMinusBtn.dataset.value === 'plus') {
+        pickGoods.order++;
+      } else {
+        pickGoods.order > 1 && pickGoods.order--;
+      }
+      //cart-page에서 수량 증감
+      goodsCount[cartdataId].value = pickGoods.order;
+      //수량에 따른 상품 가격
+      singleGoodsPrice[cartdataId].innerHTML = (
+        pickGoods.price * pickGoods.order
+      ).toLocaleString();
+      //변경사항 저장
+      saveCart(saveCartGoods);
+    }
+  });
+}
+
+//cart-page controller
+function cartListHandler(e) {
+  const plusBtns = document.querySelectorAll('.count-plus');
+  const minusBtns = document.querySelectorAll('.count-minus');
+
+  //single goods price and count
+  singleGoodsControl(e, plusBtns);
+  singleGoodsControl(e, minusBtns);
+
+  deleteCart(e);
+  totalPrice();
+  totalCartCount();
+}
+
+if (cartContainer !== null) {
+  cartContainer.addEventListener('click', cartListHandler);
+}
--- client/pages/main.js
+++ client/pages/main.js
@@ -1,7 +1,9 @@
 import '../style.css';
 import '../index.css';
 import 'flowbite';
-const data = {
+import { totalCartCount } from './cart/cart';
+
+export const data = {
   goods: [
     {
       id: 1,
@@ -48,11 +50,30 @@ const data = {
       productName: '장난감 공',
       price: 8000,
       image: '../images/Dog_ball.jpg',
-      cart: true,
+      cart: false,
+      order: 0,
+    },
+    {
+      id: 7,
+      productName: '딩동펫',
+      price: 10000,
+      image: '../images/02.png',
+      cart: false,
+      order: 0,
+    },
+    {
+      id: 8,
+      productName: '딩동펫',
+      price: 10000,
+      image: '../images/02.png',
+      cart: false,
       order: 0,
     },
   ],
+  cartlist: [{}],
 };
+
+localStorage.setItem('productList', JSON.stringify(data.goods));
 
 export function init() {
   document.querySelector('#header-wrapper').innerHTML = `<header
@@ -78,6 +99,7 @@ export function init() {
       </li>
       <li class="hover:text-gray-400">
         <a href="/cart/">장바구니</a>
+        <strong class="top-cart-count flex items-center justify-center bg-black text-white rounded-full w-5 h-5 absolute top-4 right-16"></strong>
       </li>
     </ul>
   </div>
@@ -92,11 +114,12 @@ window.addEventListener('DOMContentLoaded', () => {
 export function renderProducts() {
   const productList = document.getElementById('product-list');
 
-  const products = data.goods; // JSON 데이터에서 제품 목록을 가져옴
+  const products = JSON.parse(localStorage.getItem('productList'));
 
   // 가져온 데이터를 사용하여 동적으로 제품 목록 생성
   products.forEach((product) => {
     // 제품 항목을 생성하고 추가하는 코드
+
     const productCard = document.createElement('div');
     productCard.classList.add('product-card');
     productCard.innerHTML = `
@@ -108,10 +131,9 @@ export function renderProducts() {
           <h2 class="text-xl z-2 font-bold text-gray-900 mt-4">${product.productName}</h2>
           <div class="flex z-2 items-center justify-between mt-4">
           <span class="text-gray-900 z-2 font-bold text-lg">${product.price}원</span>
-          <button data-action='add-to-cart' class="bg-gray-900 text-white z-2 py-2 px-4 rounded-full font-bold hover:bg-gray-800" data-product-id="${product.id}">장바구니</button>
+          <button class="cart-add bg-gray-900 text-white z-2 py-2 px-4 rounded-full font-bold hover:bg-gray-800" data-product-id="${product.id}">장바구니</button>
           </div>
           </div>
-          <script src="data.json" type="home/javascript"></script>
         `;
 
     if (productList) {
@@ -120,3 +142,49 @@ export function renderProducts() {
   });
 }
 renderProducts();
+
+// 모든 장바구니 버튼에 대한 클릭 핸들러
+const buttonClickHandler = function () {
+  // 클릭한 버튼의 데이터(product-id)를 가져옴
+  const productId = this.dataset.productId;
+
+  // 해당 제품을 찾아냄
+  const selectedProduct = data.goods.find(
+    (product) => product.id === parseInt(productId),
+  );
+
+  // 이미 장바구니에 있는 상품인지 확인
+  if (saveCartGoods.some((product) => product.id === selectedProduct.id)) {
+    console.log('이미 장바구니에 있는 상품입니다.');
+    alert('장바구니에 있는 상품입니다.');
+  } else {
+    alert('장바구니에 담았습니다.');
+    selectedProduct.cart = true;
+    selectedProduct.order = 1;
+    saveCartGoods.push(selectedProduct);
+    console.log('장바구니에 추가');
+  }
+  // 장바구니 정보를 localStorage에 업데이트
+  localStorage.setItem('cartList', JSON.stringify(saveCartGoods));
+  saveCart(saveCartGoods);
+  console.log('저장완료');
+};
+
+// 모든 장바구니 버튼에 클릭 핸들러를 추가
+const cartbtns = document.querySelectorAll('.cart-add');
+cartbtns.forEach((cartbtn) => {
+  cartbtn.addEventListener('click', buttonClickHandler);
+});
+
+// localStorage에 기본 장바구니 정보를 저장
+
+export function saveCart() {
+  localStorage.setItem('cartList', JSON.stringify(saveCartGoods));
+  location.href = '/cart/';
+}
+
+export let saveCartGoods = localStorage.getItem('cartList')
+  ? JSON.parse(localStorage.getItem('cartList'))
+  : [];
+
+totalCartCount;
