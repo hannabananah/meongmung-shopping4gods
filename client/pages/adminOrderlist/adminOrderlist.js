c23d12e1c56f79e27cc374f6eb7b165c9e0284b4
[SW7] 강한나 <hanna0175@gmail.com>
2023-11-07 21:35:53 +0900

    🚧 test adminOrderlist 작업 중간커밋

--- client/pages/adminOrderlist/adminOrderlist.js
+++ client/pages/adminOrderlist/adminOrderlist.js
@@ -2,3 +2,18 @@ import '../../index.css';
 import { init } from '../main.js';
 
 init();
+
+function selectAll(selectAll) {
+  const checkboxes = document.getElementsByName('order');
+
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = selectAll.checked;
+  });
+}
+
+window.onload = function () {
+  const selectAllCheckbox = document.querySelector('input[value="selectall"]');
+  selectAllCheckbox.addEventListener('click', function () {
+    selectAll(this);
+  });
+};
