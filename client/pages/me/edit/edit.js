1020bad7284e77bbd60271f3d6aa02a2b68bd085
yulim5002 <135326595+yulim5002@users.noreply.github.com>
2023-11-10 17:17:38 +0900

    ✨ 회원관리 페이지 UI 추가 수정

--- client/pages/me/edit/edit.js
+++ client/pages/me/edit/edit.js
@@ -4,33 +4,14 @@ import Swal from 'sweetalert2';
 
 const API_BASE_URL = import.meta.env.VITE_BASE_URL;
 const token = localStorage.getItem('token');
-// const user = await getUser();
-// console.log(user);
 
-const postcode = document.querySelector('.btn-info');
 const btn = document.querySelector('form');
 const name = document.getElementById('name');
-const zipCode = document.getElementById('zipCode');
-const address = document.getElementById('address');
 const email = document.getElementById('email');
 const phone = document.getElementById('phone');
 const password = document.getElementById('password');
 const pwCheck = document.getElementById('pwCheck');
 const _id = document.getElementById('_id');
-
-console.log(zipCode);
-console.log(address);
-
-function execDaumPostcode() {
-  new daum.Postcode({
-    oncomplete: function (data) {
-      zipCode.value = data.zonecode;
-      address.value = data.address;
-    },
-  }).open();
-}
-
-postcode.addEventListener('click', execDaumPostcode);
 
 function isSame() {
   const pw = password.value;
@@ -53,7 +34,6 @@ pwCheck.addEventListener('change', isSame);
 
 const submitFrom = async () => {
   saveUser();
-  saveAddress();
 
   new Swal('수정 완료!', '회원수정이 완료되었습니다.', 'success').then(() => {
     location.href = '/me/';
@@ -71,25 +51,7 @@ const saveUser = async () => {
       name: name.value,
       email: email.value,
       phone: phone.value,
-    }),
-  })
-    .then((res) => {
-      console.log(res);
-      return res;
-    })
-    .catch((err) => console.error(err));
-};
-
-const saveAddress = async () => {
-  fetch(`${API_BASE_URL}/addresses/${_id.value}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-    body: JSON.stringify({
-      zipCode: zipCode.value,
-      detailAddress: address.value,
+      pw: password.value,
     }),
   })
     .then((res) => {
@@ -107,7 +69,6 @@ btn.addEventListener('submit', (e) => {
 document.addEventListener('DOMContentLoaded', async () => {
   init();
   getUser();
-  getAddress();
 });
 
 async function getUser() {
@@ -124,25 +85,5 @@ async function getUser() {
   name.value = data.user.name;
   email.value = data.user.email;
   phone.value = data.user.phone;
-  return data;
-}
-
-async function getAddress() {
-  const response = await fetch(`${API_BASE_URL}/addresses`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  const data = await response.json();
-  const defaultAddress = data.addresses.filter(
-    (address) => address.mainAddress,
-  );
-  console.log(defaultAddress);
-  zipCode.value = defaultAddress[0].zipCode;
-  address.value = defaultAddress[0].detailAddress;
-  _id.value = defaultAddress[0]._id;
   return data;
 }
